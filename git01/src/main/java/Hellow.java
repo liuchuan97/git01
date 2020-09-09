ab522446b82ac744fa803ff965dcd0598b3a6b6a
liuchuan97 <1826046383@qq.com>
2020-09-09 16:56:10 +0800

    Revert "修改了hello文件"
    This reverts commit 4a6686b36646bc0c4a79e7ad07396e24524a164f.

--- git01/src/main/java/Hellow.java
+++ git01/src/main/java/Hellow.java
@@ -7,5 +7,6 @@ public class Hellow {
     public static void main(String[] args) {
         System.out.println("helow");
         System.out.println("hi");
+        System.out.println("你好");
     }
 }
