46c3b4d8b1a53dcf31124615498f7bc14890d910
liuchuan97 <1826046383@qq.com>
2020-09-09 16:47:16 +0800

    创建了一个项目

---
+++ git01/src/main/java/Hellow.java
@@ -0,0 +1,10 @@
+public class Hellow {
+
+    public void hello(){
+        System.out.println("hellow");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("helow");
+    }
+}
