e0f2454b44f35e0b88356cb4888d18481fe4fdfb
liuchuan97 <1826046383@qq.com>
2020-09-09 17:02:43 +0800

    Revert "Revert "修改了hello文件""
    This reverts commit ab522446

--- git01/src/main/java/Hellow.java
+++ git01/src/main/java/Hellow.java
@@ -7,6 +7,5 @@ public class Hellow {
     public static void main(String[] args) {
         System.out.println("helow");
         System.out.println("hi");
-        System.out.println("你好");
     }
 }
